9d5cbe6e3e15b90ea825e89cb4d45a3e6348de0e
Marcel Otto <marcelotto@gmx.de>
2023-03-15 23:07:13 +0100

    Add .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,32 @@
+use RDF
+
+alias RDF.{
+  Term,
+  IRI,
+  BlankNode,
+  Literal,
+
+  Triple,
+  Quad,
+  Statement,
+
+  Description,
+  Graph,
+  Dataset,
+
+  PrefixMap,
+  PropertyMap
+  }
+
+alias RDF.BlankNode, as: BNode
+alias RDF.NS
+alias RDF.NS.{RDFS, OWL, SKOS}
+
+alias Decimal, as: D
+
+defmodule Test do
+  use RDF.Vocabulary.Namespace
+  defvocab EX, base_iri: "http://example.com/", terms: [], strict: false
+end
+
+alias Test.EX
