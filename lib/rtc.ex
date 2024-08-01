c9968fe1fde08d3b77b6c0be34059c9ce6301450
Marcel Otto <marcelotto@gmx.de>
2024-08-01 23:40:09 +0200

    Use act_as_namespace for top-level namespace

--- lib/rtc.ex
+++ lib/rtc.ex
@@ -11,6 +11,7 @@ defmodule RTC do
   """
 
   alias RDF.Resource.Generator
+  import RDF.Namespace
 
   def id, do: Generator.generate(generator_config())
   def id(args), do: Generator.generate(generator_config(), args)
@@ -19,22 +20,5 @@ defmodule RTC do
     Application.get_env(:rtc, :id, generator: RDF.BlankNode)
   end
 
-  ############################################################################
-  # These alias functions for the RTC.NS.RTC namespace are mandatory.
-  # Without them the property functions are inaccessible, since the namespace
-  # can't be aliased, because it gets in conflict with the root namespace
-  # of the project.
-
-  defdelegate elementOf(), to: RTC.NS.RTC
-  defdelegate elementOf(subject, objects), to: RTC.NS.RTC
-  defdelegate element_of(), to: RTC.NS.RTC
-  defdelegate element_of(subject, objects), to: RTC.NS.RTC
-
-  defdelegate elements(), to: RTC.NS.RTC
-  defdelegate elements(subject, objects), to: RTC.NS.RTC
-
-  defdelegate subCompoundOf(), to: RTC.NS.RTC
-  defdelegate subCompoundOf(subject, objects), to: RTC.NS.RTC
-  defdelegate sub_compound_of(), to: RTC.NS.RTC
-  defdelegate sub_compound_of(subject, objects), to: RTC.NS.RTC
+  act_as_namespace(RTC.NS.RTC)
 end
